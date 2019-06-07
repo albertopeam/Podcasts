caded5018dc57fceca68a172bfff699d00bbd36b
Alberto Penas Amor <alberto.penas.amor@mobgen.com>
2019-06-07 08:53:37 +0200

    Initial Commit

---
+++ Podcasts/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Podcasts
+//
+//  Created by Alberto on 07/06/2019.
+//  Copyright © 2019 com.github.albertopeam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
